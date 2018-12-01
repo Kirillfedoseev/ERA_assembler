205e753c8be1f660ed198ff759b16e8b7fe2eee6
oninbo <oninbo@gmail.com>
2018-12-01 16:22:17 +0300

    added putting comment token

--- ERA_Assembler/Lexer.cs
+++ ERA_Assembler/Lexer.cs
@@ -325,6 +325,24 @@ namespace ERA_Assembler
         }
 
 
+        /// <summary>
+        /// Adds comment token from the beginning of the code if finds one
+        /// </summary>
+        /// <param name="lineN"></param> number of the line
+        /// <param name="lastTokenEnd"></param> end index of the last token 
+        /// <param name="sourceCode"></param> the line of the source code to search in
+        /// <param name="tokens"></param> list of token to add operator token to
+        private void PutCommentToken(int lineN, int lastTokenEnd, string sourceCode, List<Token> tokens)
+        {
+            Regex regex = new Regex("^\\/\\/");
+            var match = regex.Match(sourceCode);
+            if (match.Success)
+            {
+                tokens.Add(new Token(TokenType.Comment, lineN, lastTokenEnd + 1, sourceCode));
+            }
+        }
+
+
         public List<Token> GetTokens(string sourceCode)
         {
             List<Token> tokens = new List<Token>();
@@ -344,6 +362,7 @@ namespace ERA_Assembler
                     PutReservedWordToken(lineN, lastTokenEnd, sourceCode, tokens);
                     PutLabelToken(lineN, lastTokenEnd, sourceCode, tokens);
                     PutStringToken(lineN, lastTokenEnd, sourceCode, tokens);
+                    PutCommentToken(lineN, lastTokenEnd, sourceCode, tokens);
                     newTokensN = tokens.Count - newTokensN;
                     if (newTokensN > 0)
                     {
